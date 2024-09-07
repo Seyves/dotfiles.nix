14fa9013e98d69b67a653da208bb28d9c5504cba
Seyves <igor906090@gmail.com>
2024-09-08 01:23:49 +0300

    removed old nvim config

--- home-manager/programs/terminal/nvim/lua/old/leap.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "ggandor/leap.nvim",
-    config = function()
-        vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward)')
-        vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward)')
-        require('leap').opts.highlight_unlabeled_phase_one_targets = true
-        require('leap').opts.case_sensitive = true
-    end
-}
