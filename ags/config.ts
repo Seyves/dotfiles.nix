cc5d6e723d4df18037b2d9863e6e3cb742e8e36e
Seyves <igor906090@gmail.com>
2024-09-09 02:24:20 +0300

    119 current  2024-09-09 02:24:17  24.05.20240903.6f6c45b  6.6.48                          *

--- ags/config.ts
+++ ags/config.ts
@@ -2,7 +2,6 @@ const hyprland = await Service.import("hyprland");
 
 function Workspaces() {
     return Widget.Box({
-        spacing: 8,
         children: [1, 2, 3, 4, 5].map((i) => {
             return Widget.Label({
                 className: "workspace",
