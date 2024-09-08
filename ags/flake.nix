737787a3a22d57d0ad428e7e5d9819db83d24809
Seyves <igor906090@gmail.com>
2024-09-08 23:24:45 +0300

    81 current  2024-09-08 23:24:44  24.05.20240903.6f6c45b  6.6.48                          *

--- ags/flake.nix
+++ ags/flake.nix
@@ -1,31 +1,28 @@
 {
   description = "Transpile ags ts into js";
 
-  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
+  outputs = { self, nixpkgs, ... }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      bun = pkgs.bun;
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        bun = pkgs.bun;
+      appBuild = pkgs.stdenv.mkDerivation {
+        name = "build-ags";
+        version = "0.1.0";
+        src = ./.;
+        buildInputs = [ bun ];
+        # https://nixos.org/manual/nixpkgs/stable/#sec-stdenv-phases
+        buildPhase = ''
+          bun build --outdir dist config.ts
+        '';
+        installPhase = ''
+          ls
+          cp -r ./dist $out
+        '';
+      };
 
-        appBuild = pkgs.stdenv.mkDerivation {
-          name = "build-ags";
-          version = "0.1.0";
-          src = ./.;
-          buildInputs = [ bun ];
-          # https://nixos.org/manual/nixpkgs/stable/#sec-stdenv-phases
-          buildPhase = ''
-            bun build --outdir dist config.ts
-          '';
-          installPhase = ''
-            ls
-            cp -r ./dist $out
-          '';
-        };
-
-      in with pkgs; {
-        defaultPackage = appBuild;
-        devShell = mkShell { buildInputs = [ bun ]; };
-      });
+    in with pkgs; {
+      defaultPackage = appBuild;
+      devShell = mkShell { buildInputs = [ bun ]; };
+    };
 }
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -27,7 +27,7 @@
     enable = true;
 
     # null or path, leave as null if you don't want hm to manage the config
-    configDir = "${inputs.ags-build-flake}";
+    configDir = "${inputs.ags-build-flake.defaultPackage}";
 
     # additional packages to add to gjs's runtime
     extraPackages = with pkgs; [ gtksourceview webkitgtk accountsservice ];
