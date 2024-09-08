0e1ca0d53c321a7c48c94b198b0bc9302dc260be
Seyves <igor906090@gmail.com>
2024-09-09 01:17:04 +0300

    92 current  2024-09-09 01:17:02  24.05.20240903.6f6c45b  6.6.48                          *

--- ags/config.ts
+++ ags/config.ts
@@ -1,20 +1,27 @@
+const hyprland = await Service.import("hyprland")
+
+function Workspaces () {
+    return Widget.Box({
+        spacing: 8,
+        children: [1, 2, 3, 4, 5].map((i) => {
+            return Widget.Label({
+                setup: (self) => {
+                    self.hook(hyprland.active.workspace, (self) => {
+                        self.label = (hyprland.active.workspace.id === i).toString()
+                    })
+                }
+            })
+        })
+    })
+}
+
 function Bar(monitor) {
-    const myVar = Variable(0)
-
-    Utils.interval(1000, () => {
-        myVar.value++
-    })
-
-    const myLabel = Widget.Label({
-        label: myVar.bind().as(v => `num: ${v}`)
-    })
-
     return Widget.Window({
         monitor,
         name: `bar${monitor}`,
         anchor: ["top", "left", "right"],
         exclusivity: "exclusive",
-        child: myLabel,
+        child: Workspaces(),
     });
 }
 
