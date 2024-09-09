cdcce60d6f549b23d4fd0d8b4f85b9e78d285c70
Seyves <igor906090@gmail.com>
2024-09-09 10:24:05 +0300

    176 current  2024-09-09 10:24:01  24.05.20240903.6f6c45b  6.6.48                          *

--- ags/config.ts
+++ ags/config.ts
@@ -6,13 +6,17 @@ function Workspaces() {
         children: [1, 2, 3, 4, 5, 6].map((i) => {
             return Widget.Label({
                 className: "workspace",
-                setup: i === 6 ? (self) => {
-                    self.hook(hyprland.active.workspace, (self) => {
-                        self.style = `margin-right: ${3 * hyprland.active.workspace.id + 8* hyprland.active.workspace.id - 1}px`
-                    });
-                } : undefined,
-        });
-    }),
+                setup:
+                    i === 6
+                        ? (self) => {
+                              self.hook(hyprland.active.workspace, (self) => {
+                                  self.style = `margin-right: ${3 * hyprland.active.workspace.id + 8 * hyprland.active.workspace.id - 1}px`;
+                              });
+                              self.class_name = "workspaces cursor";
+                          }
+                        : undefined,
+            });
+        }),
         className: "workspaces",
     });
 }
