8c152a10c76e4b97b3a85432470bc0aa1f7d76c1
Seyves <igor906090@gmail.com>
2024-09-11 02:13:46 +0300

    218 current  2024-09-11 02:13:41  24.05.20240903.6f6c45b  6.6.48                          *

--- home-manager/programs/ui/hyprland.nix
+++ home-manager/programs/ui/hyprland.nix
@@ -63,7 +63,7 @@
         # https://wiki.hyprland.org/Configuring/Variables/#blur
         blur = {
           enabled = true;
-          size = 15;
+          size = 30;
           passes = 1;
 
           vibrancy = 0.1696;
